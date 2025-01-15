5e7af4c2447f4c7d7954ec0e4c373db1ce1ef85f
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2025-01-15 09:59:26 +0700

    Add app.py

---
+++ app.py
@@ -0,0 +1,39 @@
+# test
+import uvicorn
+from fastapi import FastAPI
+from UrlData import UrlData
+from API import get_prediction
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(debug=True)
+
+# ------------------------------------------
+
+# Enabling CORS policy
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# ------------------------------------------
+
+
+@app.post("/predict")
+def predict(data: UrlData):
+    model_path = r"Malicious_URL_Prediction.h5"
+
+    prediction = get_prediction(UrlData,model_path)
+    print(prediction)
+
+    return prediction
+
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port=8000)
